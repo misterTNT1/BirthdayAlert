3e7b3ac60b5632007e205aadd91ab14be5b0fde6
eyal3 <eyal3208@gmail.com>
2024-03-11 17:53:35 +0200

    deleted the part that sends an email

--- main.py
+++ main.py
@@ -16,8 +16,4 @@ data = pd.read_csv("data/Book2.csv")
 for index, row in data.iterrows():
     name, date = row["names"], str(row["dates"])
     if date == future_Date:
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user="011eyalm@shamir.school", password=os.environ.get("password"))
-            connection.sendmail(to_addrs="eyal3208@gmail.com", from_addr="011eyalm@shamir.school", msg=f"Subject:Birthday\n\n{name} has a birthday in 2 days")
-
+        print(f"{name} has a birthday in 2 days")
