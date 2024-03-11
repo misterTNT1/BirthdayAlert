da043e027e1808a73d62cf7675fa15fa95eb95c5
eyal3 <eyal3208@gmail.com>
2024-03-11 20:31:54 +0200

    removed useless date

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 import datetime as dt
 
-current_day = dt.date.today().strftime("%#d.%#m")
 future_Date = (dt.datetime.today() + dt.timedelta(days=2)).strftime("%#d.%#m")
 
 data = pd.read_csv("data/Book2.csv")
@@ -11,4 +10,3 @@ for index, row in data.iterrows():
     name, date = row["names"], str(row["dates"])
     if date == future_Date:
         print(f"{name} has a birthday in 2 days")
-
