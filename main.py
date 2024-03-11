6c2136207d1b9947c8ef171a3a40d3de46c1df2d
eyal3 <eyal3208@gmail.com>
2024-03-11 15:53:09 +0200

    made it so it sends an email

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import smtplib
+
 import pandas as pd
 
 import datetime as dt
+
+import os
+
 
 
 current_day = dt.date.today().strftime("%#d.%#m")
@@ -11,5 +16,8 @@ data = pd.read_csv("data/Book2.csv")
 for index, row in data.iterrows():
     name, date = row["names"], str(row["dates"])
     if date == future_Date:
-        print(f"{name} has a birthday in 2 days")
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user="011eyalm@shamir.school", password=os.environ.get("password"))
+            connection.sendmail(to_addrs="eyal3208@gmail.com", from_addr="011eyalm@shamir.school", msg=f"Subject:Birthday\n\n{name} has a birthday in 2 days")
 
