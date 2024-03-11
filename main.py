9e66f90f2d8586d2fbf8bbf8415682c911096e19
eyal3 <eyal3208@gmail.com>
2024-03-11 15:28:53 +0200

    added the files

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+import datetime as dt
+
+
+current_day = dt.date.today().strftime("%#d.%#m")
+future_Date = (dt.datetime.today() + dt.timedelta(days=2)).strftime("%#d.%#m")
+
+data = pd.read_csv("data/Book2.csv")
+
+for index, row in data.iterrows():
+    name, date = row["names"], str(row["dates"])
+    if date == future_Date:
+        print(f"{name} has a birthday in 2 days")
+
